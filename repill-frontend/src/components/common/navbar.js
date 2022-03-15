92e2d986883a2180bb443e1356bc2a414fac9496
박주미 <zummy@kakao.com>
2022-03-15 23:47:32 +0900

    [S06P22A503-156] feat: navbar 구조 생성

--- repill-frontend/src/components/common/navbar.js
+++ repill-frontend/src/components/common/navbar.js
@@ -1,13 +1,32 @@
 import { Link } from 'react-router-dom';
-import React from 'react'
+import React from 'react';
+import styles from './navbar.module.css';
 
 function navbar() {
   return (
-    <div>
-      <Link to="/notice">notice</Link> ||
-      <Link to="/product">product</Link> ||
-      <Link to="/login">login</Link>
-    </div>
+    <nav className={styles.navbar}>
+      {/* logo */}
+      <Link to="/" className={styles.nav_logo}>
+        Logo
+      </Link>
+      <ul className={styles.nav_links}>
+        <li className={styles.nav_item}>
+          <Link to="/" className={styles.nav_link}>상품추천</Link>
+        </li>
+        <li className={styles.nav_item}>
+          <Link to="/product" className={styles.nav_link}>제품보기</Link>
+        </li>
+        <li className={styles.nav_item}>
+          <Link to="/notice" className={styles.nav_link}>공지사항</Link>
+        </li>
+        <li className={styles.nav_item}>
+          <Link to="/" className={styles.nav_link}>장바구니</Link>
+        </li>
+        <li className={styles.nav_item}>
+          <Link to="/login" className={styles.nav_link}>로그인</Link>
+        </li>
+      </ul>
+    </nav>
   )
 }
 

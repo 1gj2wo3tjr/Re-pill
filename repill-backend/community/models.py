b7e09757ee8b8401c93fbea35ef49a475bb20e99
조성한 <toor21@naver.com>
2022-03-15 11:21:14 +0900

    feat: 공지사항 CRUD (R만 테스트 완료)

--- repill-backend/community/models.py
+++ repill-backend/community/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.conf import settings
 
+
 class Notice(models.Model):
-    title = models.CharField(max_length=30) # 너무 작은데?
+    title = models.CharField(max_length=30)             # 30은 너무 적지 않나...?
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    views = models.IntegerField()
+    views = models.PositiveIntegerField(default=0)      # 조회수를 저장하는 데에는 어떤 자료형이 적절할까?
 
     def __str__(self):
         return f'{self.author} - {self.title}'

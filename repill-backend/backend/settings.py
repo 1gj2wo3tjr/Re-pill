d7400c5e02914b3abfd52c64e6f1277bb51874c4
조성한 <toor21@naver.com>
2022-03-16 11:34:51 +0900

    feat: swagger-ui 추가

--- repill-backend/backend/settings.py
+++ repill-backend/backend/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'drf_yasg',                     # Swagger-related
 ]
 
 MIDDLEWARE = [

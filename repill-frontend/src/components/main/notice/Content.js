ec6a6a301bd2d6881d277d577ee6aeb4f1c9c69c
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-03-16 13:27:02 +0900

    feat: "공지사항 게시글 틀 잡기"

---
+++ repill-frontend/src/components/main/notice/Content.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { TableBody, TableCell } from "@mui/material";
+
+
+function content() {
+  return (
+    <TableBody>
+      <TableCell style={{ textAlign: 'center' }}>1</TableCell>
+      <TableCell style={{ textAlign: 'center' }}>2</TableCell>
+      <TableCell style={{ textAlign: 'center' }}>3</TableCell>
+      <TableCell style={{ textAlign: 'center' }}>4</TableCell>
+      <TableCell style={{ textAlign: 'center' }}>5</TableCell>
+    </TableBody>
+  )
+}
+
+export default content

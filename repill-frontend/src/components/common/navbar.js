aad7d7cd9b5ac588d9db4e360ed8b04c5d1c914f
박주미 <zummy@kakao.com>
2022-03-16 17:23:42 +0900

    feat: 반응형 navbar 추가

--- repill-frontend/src/components/common/navbar.js
+++ repill-frontend/src/components/common/navbar.js
@@ -1,33 +1,61 @@
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './navbar.module.css';
 
-function navbar() {
+function Navbar() {
+  const [isToggled, setIsToggled] = useState(false);
+
   return (
-    <nav className={styles.navbar}>
-      {/* logo */}
-      <Link to="/" className={styles.nav_logo}>
-        Logo
-      </Link>
-      <ul className={styles.nav_links}>
-        <li className={styles.nav_item}>
-          <Link to="/" className={styles.nav_link}>상품추천</Link>
-        </li>
-        <li className={styles.nav_item}>
-          <Link to="/product" className={styles.nav_link}>제품보기</Link>
-        </li>
-        <li className={styles.nav_item}>
-          <Link to="/notice" className={styles.nav_link}>공지사항</Link>
-        </li>
-        <li className={styles.nav_item}>
-          <Link to="/" className={styles.nav_link}>장바구니</Link>
-        </li>
-        <li className={styles.nav_item}>
-          <Link to="/login" className={styles.nav_link}>로그인</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <nav className={styles.navbar}>
+        <ul className={styles.nav_logo}>
+          <Link to="/">Logo</Link>
+        </ul>
+        <ul className={styles.nav_menus} >
+          <li >
+            <Link to="/" className={styles.nav_menu}>상품추천</Link>
+          </li>
+          <li >
+            <Link to="/product" className={styles.nav_menu}>제품보기</Link>
+          </li>
+          <li >
+            <Link to="/notice" className={styles.nav_menu}>공지사항</Link>
+          </li>
+          <li >
+            <Link to="/" className={styles.nav_menu}>장바구니</Link>
+          </li>
+        </ul>
+        <ul className={styles.nav_login}>
+          <Link to="/login" className={styles.nav_menu}>로그인</Link>
+        </ul>
+        <ul className={styles.nav_toggle_btn} onClick={() => {
+          setIsToggled(!isToggled);
+        }}> <a href='#!' className={styles.nav_toggle_btn_text} >Menu</a>
+        </ul>
+      </nav >
+
+      {isToggled ? (
+        <ul className={styles.toggle_menus}>
+          <li className={styles.toggle_menu}>
+            <Link to="/" className={styles.toggle_menu_text}>상품추천</Link>
+          </li>
+          <li className={styles.toggle_menu}>
+            <Link to="/product" className={styles.toggle_menu_text}>제품보기</Link>
+          </li>
+          <li className={styles.toggle_menu}>
+            <Link to="/notice" className={styles.toggle_menu_text}>공지사항</Link>
+          </li>
+          <li className={styles.toggle_menu}>
+            <Link to="/" className={styles.toggle_menu_text}>장바구니</Link>
+          </li>
+          <li className={styles.toggle_menu}>
+            <Link to="/" className={styles.toggle_menu_text}>로그인</Link>
+          </li>
+        </ul>
+      ) : (<></>)
+      }
+    </div>
   )
 }
 
-export default navbar
+export default Navbar

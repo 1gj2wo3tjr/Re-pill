b9c103ac386b53a56c35cb379f02711d26de4aab
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-03-20 15:41:02 +0900

    feat: "상세보기 버튼 추가"

--- repill-frontend/src/components/main/notice/Content.js
+++ repill-frontend/src/components/main/notice/Content.js
@@ -1,61 +1,22 @@
 import React from 'react'
 import { TableBody, TableCell, TableRow } from "@mui/material";
+import { Link } from "react-router-dom"
 
-
-function Content({ keyword }) {
-  const content = [
-    {
-      "id": 1,
-      "title": "apple",
-      "views": 1,
-      "writer": "hjs",
-      "created": "2020.02.02"
-    },
-    {
-      "id": 2,
-      "title": "bcddd",
-      "views": 1,
-      "writer": "hjs",
-      "created": "2020.02.02"
-    },
-    {
-      "id": 3,
-      "title": "xyzza",
-      "views": 1,
-      "writer": "hjs",
-      "created": "2020.02.02"
-    },
-    {
-      "id": 4,
-      "title": "한글도 되려나",
-      "views": 1,
-      "writer": "hjs",
-      "created": "2020.02.02"
-    }
-  ]
-
-  //  props 받은 keyword 를 filter를 통해 해당 keyword를 포함하고 있는 content 들을 return 하는 함수
-  const filter = (keyword) => {
-    if (keyword !== "") {
-      return content.filter((item) => item.title.includes(keyword))
-    } else {
-      return content.map((item) => item)
-    }
-  }
-
+function Content({ list }) {
   return (
     <>
-      {filter(keyword).map((item, index) => 
-        <TableBody key={index}>
-          <TableRow>
+      <TableBody>
+        {list.map((item, index) =>
+          <TableRow key={index}>
             <TableCell style={{ textAlign: 'center' }}>{item.id}</TableCell>
             <TableCell style={{ textAlign: 'center' }}>{item.title}</TableCell>
             <TableCell style={{ textAlign: 'center' }}>{item.views}</TableCell>
             <TableCell style={{ textAlign: 'center' }}>{item.writer}</TableCell>
             <TableCell style={{ textAlign: 'center' }}>{item.created}</TableCell>
-          </TableRow>
-        </TableBody>)
-      }
+            <TableCell style={{ textAlign: 'center' }}><Link to={`/notice/${item.id}`}>상세보기</Link></TableCell>
+          </TableRow>)
+        }
+      </TableBody>
     </>
   )
 }

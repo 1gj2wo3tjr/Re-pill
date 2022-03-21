94113d34eef76cfd969c1cb09508e139219028ed
박주미 <zummy@kakao.com>
2022-03-22 01:47:37 +0900

    feat: 제품 카테고리 레이아웃 추가

--- repill-frontend/src/components/main/product/product.js
+++ repill-frontend/src/components/main/product/product.js
@@ -1,22 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { Container } from 'semantic-ui-react'
-import Navbar from "../../common/navbar"
 import styles from "./Product.module.css"
 import SearchIcon from '@mui/icons-material/Search';
 import axios from 'axios';
 import ProductList from './ProductList'
+import Category from './Category'
 import Paging from './Pagination'
 import './Pagination.module.css';
 
 function Product() {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentpage] = useState(1);
-  const [postPerPage] = useState(50);
+  const [postPerPage] = useState(40);
 
   const getProducts = async () => {
     const response = await axios.get("https://jsonplaceholder.typicode.com/photos");
-
     setProducts(response.data);
   }
 
@@ -34,9 +32,10 @@ function Product() {
 
   return (
     <div>
-      <Navbar />
-      <Container>
-        <h1>제품 보기</h1>
+      <div style={{ display: "inline" }}>
+        <Category />
+      </div>
+      <Container className={styles.container}>
         <div className={styles.search_div}>
           <>
             <input type="text" placeholder='찾으시는 제품을 검색해주세요.' className={styles.search_input}></input>

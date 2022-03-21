c856275db43253d06895878cf3f1a6813b4ede86
jwkim.ap <jwkim.ap@gmail.com>
2022-03-21 22:47:05 +0900

    fix: 인가 코드로 access token 받는 로직 추가

--- repill-backend/accounts/views.py
+++ repill-backend/accounts/views.py
@@ -11,12 +11,25 @@ import requests
 # Create your views here.
 class KakaoLogin(View):
     def get(self, request):
-        kakao_access_token = request.headers["Authorization"]  # frontend에서 받은 토큰
+        auth_code = request.GET.get("code")
+        kakao_token_api = "https://kauth.kakao.com/oauth/token"
+        data = {
+            "grant_type": "authorization_code",
+            "client_id": "600acda95b91e8c309764ed293d14bf5",
+            "redirection_uri": "http://localhost:8000/accounts/login/callback",
+            "code": auth_code,
+        }
+
+        token_response = requests.post(kakao_token_api, data = data).json()
+
+        kakao_access_token = token_response.get("access_token")
         headers = ({"Authorization": f"Bearer {kakao_access_token}"})
-        url = "https://kapi.kakao.com/v1/user/me"  # 회원 정보 확인을 위한 카카오 API 주소
+        url = "https://kapi.kakao.com/v2/user/me"  # 회원 정보 확인을 위한 카카오 API 주소
         social_user = requests.request("POST", url, headers=headers).json()
 
-        if User.objects.filter(uid = social_user["id"]).exists():  # DB에 회원정보 있으면 로그인
+        print(social_user)
+
+        if User.objects.filter(uid = social_user.get("id")).exists():  # DB에 회원정보 있으면 로그인
             user_info = User.objects.get(uid = social_user["id"])
             jwt_token = jwt.encode({"uid": social_user.id}, "secret_key")  # JWT 발행
             status = 200

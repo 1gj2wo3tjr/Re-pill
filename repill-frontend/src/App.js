c9f2a83f0489dd08fcbf919a2e559c517e8e66a3
김동현 <budkim1124@naver.com>
2022-03-23 14:24:41 +0900

    feature/frontend/login

--- repill-frontend/src/App.js
+++ repill-frontend/src/App.js
@@ -1,17 +1,13 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom"
-import Main from "./pages/main/main"
-import Notice from "./components/main/notice/Notice"
-import Product from "./components/main/product/product"
-import NoticeDetail from "./components/main/notice/NoticeDetail"
-import "./index.css"
-import Navbar from "../src/components/common/navbar"
-import Footer from "../src/components/common/footer"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./pages/main/main";
+import Notice from "./components/main/notice/Notice";
+import Product from "./components/main/product/product";
+import NoticeDetail from "./components/main/notice/NoticeDetail";
+import "./index.css";
+import Navbar from "../src/components/common/navbar";
+import Footer from "../src/components/common/footer";
 import KakaoLogin from "./components/main/login/kakaologin";
-
+import Auth from "./components/main/login/Auth";
 
 function App() {
   return (
@@ -23,7 +19,7 @@ function App() {
           <Route path="/notice" element={<Notice />} />
           <Route path="/product" element={<Product />} />
           <Route path="/kakaologin" exact element={<KakaoLogin />} />
-        <Route path="/oauth/callback/kakao" element={<Auth />} />
+          <Route path="/oauth/callback/kakao" element={<Auth />} />
           <Route path="/notice/:id" element={<NoticeDetail />} />
         </Routes>
       </div>

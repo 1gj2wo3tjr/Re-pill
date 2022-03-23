1cbdc1da72e2402382e35cf6eb3b34dc0d1114d3
조성한 <toor21@naver.com>
2022-03-23 14:58:55 +0900

    chore: delete filters.py

--- repill-backend/community/filters.py
+++
@@ -1,7 +0,0 @@
-from django_filters import FilterSet
-from .models import Notice
-
-class NoticeFilter(FilterSet):
-    class Meta:
-        model = Notice
-        fields = ['author', 'title', 'content']

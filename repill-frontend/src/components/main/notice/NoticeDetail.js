ca9d4181ebc16b74d89f8d299068f5e3a04c0523
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-03-27 23:22:26 +0900

    style: "공지사항 상세페이지 반응형 추가"

--- repill-frontend/src/components/main/notice/NoticeDetail.js
+++ repill-frontend/src/components/main/notice/NoticeDetail.js
@@ -5,6 +5,7 @@ import { useParams, useNavigate, Link } from "react-router-dom";
 import styles from './Notice.module.css';
 import axios from "axios";
 import EditNoticeModal from './EditNoticeModal';
+import { useMediaQuery } from 'react-responsive';
 
 function NoticeDetail() {
   let params = useParams()
@@ -23,9 +24,15 @@ function NoticeDetail() {
   const [previousTitle, setPreviousTitle] = useState()
   const [nextTitle, setNextTitle] = useState()
 
+  const isMobile = useMediaQuery({
+    query: "(max-width : 768px)"
+  });
+
   const getDetail = async () => {
     const response = await axios.get(`http://127.0.0.1:8000/api/v1/community/notice/${params.id}`)
+    console.log(response.data)
     setDetail(response.data)
+    console.log(detail)
     if (response.data.cursor.previous !== null) {
       setPreviousId(response.data.cursor.previous.id)
       setPreviousTitle(response.data.cursor.previous.title)
@@ -50,6 +57,19 @@ function NoticeDetail() {
     setRemove((prev) => !prev)
   }
 
+  const checkDelete = () => {
+    setRemove(false)
+    // axios delete 요청 코드
+    axios.delete(`http://127.0.0.1:8000/api/v1/community/notice/${params.id}`)
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+    navigate('/notice')
+  }
+
+  const checkCancle = () => {
+    setRemove(false)
+  }
+
   useEffect(() => {
     getDetail()
     getLength()
@@ -58,71 +78,142 @@ function NoticeDetail() {
 
   return (
     <>
-      <Container>
-        <h2 style={{ marginTop: "5%" }}>공지사항</h2>
-        {user==="admin" ? (
-          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <button className={styles.edit_notice_button} onClick={openModal}>수정하기</button>
-            <button className={styles.delete_notice_button} onClick={openDeleteModal}>삭제하기</button>
-          </div>) : null}
-        <Table style={{ marginTop: "3%" }}>
-          <TableHead>
-            <TableRow style={{ backgroundColor: '#F2F5C8', boxShadow: "0px 5px 10px rgb(207 206 206)" }}>
-              <TableCell style={{ display: "grid", gridTemplateColumns: "10% 80% 10%" }}>
-                <div></div>
-                <div className={styles.notice_title}>{detail.title}</div>
-                <div className={styles.notice_date}>{slicedDate}</div>
-              </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow style={{ height: "30rem" }}>
-              <TableCell style={{ fontSize: "2rem", textAlign: "center" }}>
-                {detail.content}
-              </TableCell>
-            </TableRow>
-            {params.id==="1000" ? (null) : (
-              <>
-                <TableRow className={styles.notice_link}>
-                  <TableCell style={{ display: "flex" }}>
-                    <div>이전글</div>
-                    <div style={{ margin: "auto" }}><Link to={`/notice/${previousId}`} style={{ color: "inherit" }}>{previousTitle}</Link></div>
+      {isMobile ? (
+        <>
+          <Container>
+            <h3 style={{ marginTop: "5%" }}>공지사항</h3>
+            {user==="admin" ? (
+              <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <button className={styles.edit_notice_button_mob} onClick={openModal}>수정하기</button>
+                <button className={styles.delete_notice_button_mob} onClick={openDeleteModal}>삭제하기</button>
+              </div>) : null}
+            <Table style={{ marginTop: "3%" }}>
+              <TableHead>
+                <TableRow style={{ backgroundColor: '#F2F5C8', boxShadow: "0px 5px 10px rgb(207 206 206)" }}>
+                  <TableCell style={{ display: "grid", gridTemplateColumns: "20% 60% 20%" }}>
+                    <div></div>
+                    <div className={styles.notice_title_mob}>{detail.title}</div>
+                    <div className={styles.notice_date_mob}>{slicedDate}</div>
                   </TableCell>
                 </TableRow>
-              </>)}
-            {params.id==="1000" ? (null) : (
-              <>
-                <TableRow className={styles.notice_link}>
-                  <TableCell style={{ display: "flex"}}>
-                    <div>다음글</div>
-                    <div style={{ margin: "auto" }}><Link to={`/notice/${parseInt(nextId)}`} style={{ color: "inherit" }}>{nextTitle}</Link></div>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell style={{ fontSize: "1.4rem", textAlign: "center" }}>
+                    {detail.content}
                   </TableCell>
                 </TableRow>
-              </>)}
-          </TableBody>
-        </Table>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <button className={styles.goNotice_button} onClick={goNotice}>목록보기</button>
-        </div>
-      </Container>
-      <EditNoticeModal open={open} setOpen={setOpen} title={detail.title} content={detail.body} />
-      {/* 삭제하기 버튼 클릭 시 확인용 모달 생성 */}
-      <Modal
-        size='tiny'
-        open={remove}
-      >
-        <Header icon='archive' content='공지사항을 삭제하시겠습니까?' />
-        <Modal.Actions>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <Button color='green' onClick={() => setRemove(false)}>
-              <Icon name='checkmark' /> 삭제
-            </Button>
-            <Button color='red' onClick={() => setRemove(false)}>
-              <Icon name='remove' /> 취소
-            </Button>
-          </div>
-        </Modal.Actions>
-      </Modal>
+                {params.id==="1000" ? (null) : (
+                  <>
+                    <TableRow className={styles.notice_link}>
+                      <TableCell style={{ display: "flex" }}>
+                        <div>이전글</div>
+                        <div style={{ margin: "auto" }}><Link to={`/notice/${previousId}`} style={{ color: "inherit" }}>{previousTitle}</Link></div>
+                      </TableCell>
+                    </TableRow>
+                  </>)}
+                {params.id==="1000" ? (null) : (
+                  <>
+                    <TableRow className={styles.notice_link}>
+                      <TableCell style={{ display: "flex"}}>
+                        <div>다음글</div>
+                        <div style={{ margin: "auto" }}><Link to={`/notice/${parseInt(nextId)}`} style={{ color: "inherit" }}>{nextTitle}</Link></div>
+                      </TableCell>
+                    </TableRow>
+                  </>)}
+              </TableBody>
+            </Table>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <button className={styles.goNotice_button_mob} onClick={goNotice}>목록보기</button>
+            </div>
+          </Container>
+          <EditNoticeModal open={open} setOpen={setOpen} id={params.id} />
+          {/* 취소 확인 모달 */}
+          <Modal
+            size='tiny'
+            open={remove}
+            style={{ width: "80%" }}
+          >
+            <Header icon='archive' content='공지사항을 삭제하시겠습니까?' style={{ fontSize: "1.1rem" }} />
+            <Modal.Actions>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button color='green' onClick={checkDelete}>
+                  <Icon name='checkmark' /> 삭제
+                </Button>
+                <Button color='red' onClick={checkCancle}>
+                  <Icon name='remove' /> 취소
+                </Button>
+              </div>
+            </Modal.Actions>
+          </Modal>
+        </>) : (
+        <>
+          <Container>
+            <h2 style={{ marginTop: "5%" }}>공지사항</h2>
+            {user==="admin" ? (
+              <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <button className={styles.edit_notice_button} onClick={openModal}>수정하기</button>
+                <button className={styles.delete_notice_button} onClick={openDeleteModal}>삭제하기</button>
+              </div>) : null}
+            <Table style={{ marginTop: "3%" }}>
+              <TableHead>
+                <TableRow style={{ backgroundColor: '#F2F5C8', boxShadow: "0px 5px 10px rgb(207 206 206)" }}>
+                  <TableCell style={{ display: "grid", gridTemplateColumns: "10% 80% 10%" }}>
+                    <div></div>
+                    <div className={styles.notice_title}>{detail.title}</div>
+                    <div className={styles.notice_date}>{slicedDate}</div>
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow style={{ height: "30rem" }}>
+                  <TableCell style={{ fontSize: "2rem", textAlign: "center" }}>
+                    {detail.content}
+                  </TableCell>
+                </TableRow>
+                {params.id==="1000" ? (null) : (
+                  <>
+                    <TableRow className={styles.notice_link}>
+                      <TableCell style={{ display: "flex" }}>
+                        <div>이전글</div>
+                        <div style={{ margin: "auto" }}><Link to={`/notice/${previousId}`} style={{ color: "inherit" }}>{previousTitle}</Link></div>
+                      </TableCell>
+                    </TableRow>
+                  </>)}
+                {params.id==="1000" ? (null) : (
+                  <>
+                    <TableRow className={styles.notice_link}>
+                      <TableCell style={{ display: "flex"}}>
+                        <div>다음글</div>
+                        <div style={{ margin: "auto" }}><Link to={`/notice/${parseInt(nextId)}`} style={{ color: "inherit" }}>{nextTitle}</Link></div>
+                      </TableCell>
+                    </TableRow>
+                  </>)}
+              </TableBody>
+            </Table>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <button className={styles.goNotice_button} onClick={goNotice}>목록보기</button>
+            </div>
+          </Container>
+          <EditNoticeModal open={open} setOpen={setOpen} id={params.id} />
+          
+          <Modal
+            size='tiny'
+            open={remove}
+          >
+            <Header icon='archive' content='공지사항을 삭제하시겠습니까?' />
+            <Modal.Actions>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <Button color='green' onClick={() => setRemove(false)}>
+                  <Icon name='checkmark' /> 삭제
+                </Button>
+                <Button color='red' onClick={() => setRemove(false)}>
+                  <Icon name='remove' /> 취소
+                </Button>
+              </div>
+            </Modal.Actions>
+          </Modal>
+        </>)}
     </>
   )
 }

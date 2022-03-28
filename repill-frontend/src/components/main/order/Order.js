cbb24aee8d3638cfc09ea4b1d5e4e05d40c4562c
박주미 <zummy@kakao.com>
2022-03-28 17:23:48 +0900

    style: 주문/결제 반응형 적용

--- repill-frontend/src/components/main/order/Order.js
+++ repill-frontend/src/components/main/order/Order.js
@@ -49,7 +49,80 @@ function Order() {
 
   return (
     <>
-      {isMobile ? null : (
+      {isMobile ? (
+        <div>
+          <Container className={styles.mob_container}>
+            <div className={styles.mob_top}>
+              <h3>주문/결제</h3>
+            </div>
+            <div className={styles.mob_order_main}>
+              <Table>
+                <TableHead style={{ padding: "0px" }}>
+                  <TableCell></TableCell>
+                </TableHead>
+                <TableBody>
+                  <TableCell>
+                    <div className={styles.mob_order_list}>
+                      <img
+                        alt=""
+                        src="https://shopping-phinf.pstatic.net/main_2877420/28774205554.20210909130858.jpg?type=f300"
+                      />
+                      <div className={styles.mob_order_view}>
+                        <p>회사명</p>
+                        <p>상품명</p>
+                      </div>
+                    </div>
+                  </TableCell>
+                </TableBody>
+              </Table>
+            </div>
+            <div></div>
+            <div className={styles.mob_order_bottom}>
+              <div className={styles.mob_final_pay}>
+                <p>최종 결제 금액</p>
+                <p
+                  style={{
+                    color: "#219f94",
+                    fontSize: "25px",
+                    lineHeight: "25px",
+                    marginLeft: "10px",
+                  }}
+                >
+                  50,000원
+                </p>
+              </div>
+              <div className={styles.mob_agreement}>
+                <Checkbox
+                  aria-label="a"
+                  sx={{
+                    color: "#cfcfcf",
+                    "&.Mui-checked": {
+                      color: "#219F94",
+                    },
+                  }}
+                />
+                <div style={{ fontSize: "12px", margin: "10px" }}>
+                  <p style={{ margin: "0px" }}>
+                    <span style={{ fontWeight: "bold" }}>(필수)</span> 구매하실
+                    상품의 결제정보를 확인하였으며,
+                  </p>
+                  <p>구매진행에 동의합니다.</p>
+                </div>
+              </div>
+              <Link to={`/orderCompleted`}>
+                <button className={styles.mob_button_buy}>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <CreditScoreIcon
+                      style={{ marginRight: "10px" }}
+                    ></CreditScoreIcon>
+                    <p>결제하기</p>
+                  </div>
+                </button>
+              </Link>
+            </div>
+          </Container>
+        </div>
+      ) : (
         <div>
           <Container className={styles.container}>
             <div className={styles.order_top}>

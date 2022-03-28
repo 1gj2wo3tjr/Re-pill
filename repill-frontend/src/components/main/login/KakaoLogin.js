5a2498618a10dec5285131f0dee3ff6d291d63d5
김동현 <budkim1124@naver.com>
2022-03-28 16:28:49 +0900

    fixed:login form

--- repill-frontend/src/components/main/login/KakaoLogin.js
+++ repill-frontend/src/components/main/login/KakaoLogin.js
@@ -1,18 +1,36 @@
 import React from "react";
+import "./styles.css";
 
 const KakaoLogin = () => {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
   return (
-    <div>
-      <h1>
-        <a href={KAKAO_AUTH_URL}>
-          <img
-            src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
-            style={{ width: 200 }}
-          ></img>
-        </a>
-      </h1>
-    </div>
+    // <div style={{ padding: "0px" }}>
+    //   <div style={{ border: "10px solid black", height: "100%", width: "20%" }}>
+    //     <a href={KAKAO_AUTH_URL}>
+    //       <img
+    //         src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
+    //         style={{ width: 200, marginLeft: "45%", marginTop: "20%" }}
+    //       ></img>
+    //     </a>
+    //   </div>
+    // </div>
+    <>
+      <div class="login-box">
+        <h2>Login</h2>
+        <form>
+          <a href={KAKAO_AUTH_URL}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <img
+              src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
+              style={{ width: 280 }}
+            ></img>
+          </a>
+        </form>
+      </div>
+    </>
   );
 };
 export default KakaoLogin;

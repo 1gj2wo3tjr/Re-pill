876817e20701c55d42e949de67161e428aa2d0ff
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-04-05 00:33:15 +0900

    chore: "community views.py paginator 코드 삭제"

--- repill-backend/community/views.py
+++ repill-backend/community/views.py
@@ -48,26 +48,11 @@ from django.core.cache import cache
 def notice_list(request):
 
     def get_notice_list(request):  
-
-        paginator = PageNumberPagination()
-        paginator.page_size = 10
-        
-
         # 공지를 PK 역순으로 표시
         notices = Notice.objects.order_by('-pk')
 
-        
-        query = request.GET.get('search', None)
-        if query:
-            notices = get_list_or_404(notices.filter(title__contains=query))
-        else:
-            notices = get_list_or_404(notices)
-        
-        page = paginator.paginate_queryset(notices, request)
-        serializer = NoticeListSerializer(page, many=True)
-
-
-        return paginator.get_paginated_response(serializer.data)
+        serializer = NoticeListSerializer(notices, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post_notice(request):
         if not request.user.is_authenticated:

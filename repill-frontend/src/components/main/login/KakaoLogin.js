ccd3e3761ee43ef419d49218ec3a08afbb60e9f3
김동현 <budkim1124@naver.com>
2022-04-05 14:59:04 +0900

    fixed:userinfo

--- repill-frontend/src/components/main/login/KakaoLogin.js
+++ repill-frontend/src/components/main/login/KakaoLogin.js
@@ -4,16 +4,6 @@ import "./styles.css";
 const KakaoLogin = () => {
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_REST_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&response_type=code`;
   return (
-    // <div style={{ padding: "0px" }}>
-    //   <div style={{ border: "10px solid black", height: "100%", width: "20%" }}>
-    //     <a href={KAKAO_AUTH_URL}>
-    //       <img
-    //         src="//k.kakaocdn.net/14/dn/btqCn0WEmI3/nijroPfbpCa4at5EIsjyf0/o.jpg"
-    //         style={{ width: 200, marginLeft: "45%", marginTop: "20%" }}
-    //       ></img>
-    //     </a>
-    //   </div>
-    // </div>
     <>
       <div class="login-box">
         <h2>Login</h2>

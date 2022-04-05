49cdd773bf604de2d72f70e38d7361ee067789d1
jwkim.ap <jwkim.ap@gmail.com>
2022-04-05 16:23:12 +0900

    feat: accounts 앱 Swagger 적용

--- repill-backend/accounts/views.py
+++ repill-backend/accounts/views.py
@@ -1,6 +1,8 @@
 from django.http import JsonResponse
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django.views import View
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -74,12 +76,16 @@ class KakaoLogin(View):
 # 배송지 관련 API
 class AddressList(APIView):
     # 특정 사용자의 배송지 전체 조회
+    @swagger_auto_schema(operation_description="특정 사용자의 모든 배송지를 조회합니다.")
     def get(self, request):
         addresses = DeliveryAddress.objects.filter(user=request.user)
         serializer = DeliveryAddressSerializer(addresses, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     # 배송지 저장
+    @swagger_auto_schema(operation_description="신규 배송지를 저장합니다.",
+        request_body=DeliveryAddressSerializer,
+        responses={201: DeliveryAddressSerializer, 400: "올바르지 않은 데이터값"})
     def post(self, request):
         serializer = DeliveryAddressSerializer(data=request.data)
         if serializer.is_valid():
@@ -90,6 +96,8 @@ class AddressList(APIView):
 
 class AddressDetail(APIView):
     # 특정 배송지 조회
+    @swagger_auto_schema(operation_description="특정 배송지를 조회합니다.",
+        responses={200: DeliveryAddressSerializer, 403: "조회 권한이 없음 (사용자 불일치)", 404: "해당 배송지를 찾을 수 없음"})
     def get(self, request, address_pk):
         try:
             address = get_object_or_404(DeliveryAddress, pk=address_pk)
@@ -102,26 +110,39 @@ class AddressDetail(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
     # 배송지 정보 수정
+    @swagger_auto_schema(operation_description="특정 배송지를 수정합니다.",
+        request_body=DeliveryAddressSerializer,
+        responses={200: DeliveryAddressSerializer, 400: "올바르지 않은 데이터값", 404: "해당 배송지를 찾을 수 없음"})
     def put(self, request, address_pk):
-        address = DeliveryAddress.objects.get(pk=address_pk)
-        serializer = DeliveryAddressSerializer(address, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            address = get_object_or_404(DeliveryAddress, pk=address_pk)
+            serializer = DeliveryAddressSerializer(address, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
     # 배송지 삭제
+    @swagger_auto_schema(operation_description="특정 배송지를 삭제합니다.",
+        responses={204: "배송지가 성공적으로 삭제됨", 403: "배송지 삭제 권한이 없음", 404: "해당 배송지를 찾을 수 없음"})
     def delete(self, request, address_pk):
-        address = DeliveryAddress.objects.get(pk=address_pk)
-        if address.user == request.user:
-            address.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        return Response(status=status.HTTP_403_FORBIDDEN)
+        try: 
+            address = get_object_or_404(DeliveryAddress, pk=address_pk)
+            if address.user == request.user or request.user.is_staff:
+                address.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
     
 
 # 주문 관련 API
 class OrderList(APIView):
     # 특정 사용자의 주문 전체 조회
+    @swagger_auto_schema(operation_description="특정 사용자의 모든 주문 내역을 조회합니다.",
+        responses={200: OrderSerializer(many=True), 401: "인증된 유저가 아님 (로그인 필요)"})
     def get(self, request):
         if request.user.is_authenticated:    
             orders = Order.objects.filter(user=request.user)
@@ -130,6 +151,17 @@ class OrderList(APIView):
         return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
     # 주문 접수
+    @swagger_auto_schema(operation_description="주문을 생성합니다.", request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT, 
+        properties={
+            'products':openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type="dict", format="number: 상품번호, quantity: 상품개수"), description="제품 번호 및 수량"),
+            'address': openapi.Schema(type=openapi.TYPE_STRING, description='주소'),
+            'order_status': openapi.Schema(type=openapi.TYPE_STRING, description='주문 상태'),
+            'order_receive': openapi.Schema(type=openapi.TYPE_STRING, description='수령인')
+            },
+        ),
+        responses={201: OrderSerializer(many=True), 400: "올바르지 않은 데이터값", 401: "인증된 유저가 아님 (로그인 필요)"}
+    )
     def post(self, request):
         order_data = []
 
@@ -154,6 +186,8 @@ class OrderList(APIView):
 
 class OrderDetail(APIView):
     # 특정 주문 조회
+    @swagger_auto_schema(operation_description="특정 주문을 조회합니다.",
+        responses={200: OrderSerializer, 401: "인증된 유저가 아님 (로그인 필요)", 404: "해당 주문을 찾을 수 없음"})
     def get(self, request, order_pk):
         if request.user.is_authenticated:
             try:
@@ -168,11 +202,14 @@ class OrderDetail(APIView):
     주문 수정 (배송지 변경, 주문 상태 변경 등)
     주문 취소도 PUT 요청으로 (order_status를 0으로 변경)
     '''
+    @swagger_auto_schema(operation_description="주문을 수정합니다.",
+        request_body=OrderSerializer,
+        responses={200: OrderSerializer, 401: "인증된 유저가 아님 (로그인 필요)", 404: "해당 배송지를 찾을 수 없음"})
     def put(self, request, order_pk):
         if request.user.is_authenticated:
             try:
                 order_part = Order.objects.filter(order_number=order_pk).filter(product=request.data.get("product")).first()
-                if not request.user == order_part.user:
+                if not request.user.is_staff and not request.user == order_part.user:
                     return Response(status=status.HTTP_403_FORBIDDEN)    
                 else:    
                     serializer = OrderSerializer(order_part, data=request.data)
@@ -188,6 +225,8 @@ class OrderDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
     # 주문 기록 삭제 (관리자)
+    @swagger_auto_schema(operation_description="주문 기록을 삭제합니다 (관리자 전용 기능)",
+        responses={204: "주문 기록 성공적으로 삭제", 403: "관리자 권한이 없음"})
     def delete(self, request, order_pk):
         if request.user.is_staff:
             orders = get_list_or_404(Order, pk=order_pk)
@@ -200,6 +239,8 @@ class OrderDetail(APIView):
 # 구독 관련 API
 class SubscriptionList(APIView):
     # 특정 사용자의 구독 전체 조회
+    @swagger_auto_schema(operation_description="특정 사용자의 모든 구독을 조회합니다.",
+        responses={200: SubscriptionSerializer(many=True), 401: "인증된 유저가 아님 (로그인 필요)"})
     def get(self, request):
         if request.user.is_authenticated:
             subscriptions = Subscription.objects.filter(user=request.user)
@@ -208,6 +249,16 @@ class SubscriptionList(APIView):
         return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
     # 구독 신청
+    @swagger_auto_schema(operation_description="특정 상품의 구독을 신청합니다.",
+        request_body = openapi.Schema(
+            type=openapi.TYPE_OBJECT, 
+            properties={
+            'product': openapi.Schema(type=openapi.TYPE_INTEGER, description='상품 번호'),
+            'period': openapi.Schema(type=openapi.TYPE_INTEGER, description='구독 간격 (일수)'),
+            'subscribe_times': openapi.Schema(type=openapi.TYPE_INTEGER, description='수령인')
+            },
+        ),
+        responses={201: SubscriptionSerializer, 400: "올바른 데이터값이 아님", 401: "인증된 유저가 아님 (로그인 필요)"})
     def post(self, request):
         if request.user.is_authenticated:
             serializer = SubscriptionSerializer(data=request.data)
@@ -226,11 +277,13 @@ class SubscriptionList(APIView):
                 serializer.save(user=request.user, subscribe_dates=subscribe_dates)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
 
 
 class SubscriptionDetail(APIView):
     # 특정 상품 구독 조회
+    @swagger_auto_schema(operation_description="특정 사용자의 특정 상품 구독을 조회합니다.",
+        responses={200: SubscriptionSerializer, 401: "인증된 유저가 아님 (로그인 필요)", 404: "해당 상품의 구독 정보가 없음"})
     def get(self, request, product_pk):
         if request.user.is_authenticated:
             try:
@@ -242,6 +295,9 @@ class SubscriptionDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
     # 상품 구독 수정
+    @swagger_auto_schema(operation_description="특정 사용자의 특정 상품 구독을 조회합니다.",
+        request_body = SubscriptionSerializer,
+        responses={200: SubscriptionSerializer, 400: "올바르지 않은 데이터값", 401: "인증된 유저가 아님 (로그인 필요)", 404: "해당 상품의 구독 정보가 없음"})
     def put(self, request, product_pk):
         if request.user.is_authenticated:
             try:
@@ -256,6 +312,8 @@ class SubscriptionDetail(APIView):
         return Response(serializer.errors, status=status.HTTP_401_UNAUTHORIZED)
 
     # 상품 구독 취소
+    @swagger_auto_schema(operation_description="특정 사용자의 구독을 취소합니다.",
+    responses={204: "구독 취소 성공", 401: "인증된 유저가 아님 (로그인 필요)", 404: "해당 상품의 구독 정보가 없음"})
     def delete(self, request, product_pk):
         if request.user.is_authenticated:
             try:
@@ -264,4 +322,4 @@ class SubscriptionDetail(APIView):
                 return Response(status=status.HTTP_204_NO_CONTENT)
             except:
                 return Response(status=status.HTTP_404_NOT_FOUND)
-        return Response(status=status.HTTP_403_FORBIDDEN)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)

14d156892794af93d6abfb1fe7128e6173975a30
박주미 <zummy@kakao.com>
2022-04-06 04:17:41 +0900

    style: navbar 스타일 수정

--- repill-frontend/src/components/common/Navbar.js
+++ repill-frontend/src/components/common/Navbar.js
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState, useEffect } from "react";
 import styles from "./Navbar.module.css";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Navbar() {
   const [isToggled, setIsToggled] = useState(false);
@@ -20,58 +21,121 @@ function Navbar() {
     navigate("/");
   };
 
+  // const [keyword, setKeyword] = useState("");
+  // const onChange = (e) => {
+  //   console.log(e.target.value);
+  //   setKeyword(e.target.value);
+  // };
+
+  // const onClick = () => {
+  //   // window.location.reload(true);
+  //   navigate(`/product`, { state: { keyword: keyword } });
+  //   setKeyword("");
+  // };
+
   return (
     <>
       {login ? (
-        <div>
-          <nav className={styles.navbar}>
-            <ul className={styles.nav_logo}>
-              <Link to="/">Logo</Link>
-            </ul>
-            <ul className={styles.nav_menus}>
-              <li>
-                <Link to="/" className={styles.nav_menu}>
-                  상품추천
-                </Link>
-              </li>
-              <li>
-                <Link to="/product" className={styles.nav_menu}>
-                  제품보기
-                </Link>
-              </li>
-              <li>
-                <Link to="/notice" className={styles.nav_menu}>
-                  공지사항
-                </Link>
-              </li>
-              <li>
-                <Link to="/cart" className={styles.nav_menu}>
-                  장바구니
-                </Link>
-              </li>
-              <li>
-                <Link to="/mypage" className={styles.nav_menu}>
-                  마이페이지
-                </Link>
-              </li>
-            </ul>
-            <ul className={styles.nav_login}>
-              <span style={{ cursor: "pointer" }} onClick={clickLogout}>
-                로그아웃
-              </span>
-            </ul>
-            <ul
-              className={styles.nav_toggle_btn}
-              onClick={() => {
-                setIsToggled(!isToggled);
-              }}
-            >
-              {" "}
-              <a href="#!" className={styles.nav_toggle_btn_text}>
-                Menu
-              </a>
-            </ul>
-          </nav>
+        <div className={styles.navbar_main}>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <nav className={styles.navbar_top}>
+              <ul className={styles.navbar_top_ul}>
+                {/* <li>
+                  <Link to="/notice" className={styles.navbar_top_list}>
+                    공지사항
+                  </Link>
+                </li> */}
+                <li>
+                  <Link to="/mypage" className={styles.navbar_top_list}>
+                    마이페이지
+                  </Link>
+                </li>
+                <li>
+                  <span
+                    style={{ cursor: "pointer" }}
+                    onClick={clickLogout}
+                    className={styles.navbar_top_list}
+                  >
+                    로그아웃
+                  </span>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Link to="/">
+              <img src="/img/logo_1.png" style={{ width: "140px" }} />
+            </Link>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <nav className={styles.navbar}>
+              <div>
+                <ul className={styles.nav_menus}>
+                  <li>
+                    <Link to="/recommend" className={styles.nav_menu}>
+                      상품추천
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/product" className={styles.nav_menu}>
+                      제품보기
+                    </Link>
+                    {/* <p
+                      className={styles.nav_menu}
+                      onClick={onClick}
+                      style={{ cursor: "pointer" }}
+                    >
+                      제품보기
+                    </p> */}
+                  </li>
+                  {/* <li>
+                    <div className={styles.nav_menu}>
+                      <div
+                        style={{ display: "flex", justifyContent: "center" }}
+                      >
+                        <input
+                          type="text"
+                          placeholder="찾으시는 제품을 검색해주세요."
+                          className={styles.search_input}
+                          onChange={onChange}
+                          value={keyword}
+                        ></input>
+                        <button className={styles.search_btn} onClick={onClick}>
+                          <SearchIcon style={{ fontSize: "25px" }}></SearchIcon>
+                        </button>
+                      </div>
+                    </div>
+                  </li> */}
+                  <li>
+                    <Link to="/notice" className={styles.nav_menu}>
+                      공지사항
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/cart" className={styles.nav_menu}>
+                      장바구니
+                    </Link>
+                  </li>
+                </ul>
+                <ul
+                  className={styles.nav_toggle_btn}
+                  onClick={() => {
+                    setIsToggled(!isToggled);
+                  }}
+                >
+                  {" "}
+                  <a href="#!" className={styles.nav_toggle_btn_text}>
+                    Menu
+                  </a>
+                </ul>
+              </div>
+            </nav>
+          </div>
 
           {isToggled ? (
             <ul className={styles.toggle_menus}>
@@ -106,56 +170,71 @@ function Navbar() {
           )}
         </div>
       ) : (
-        <div>
-          <nav className={styles.navbar}>
-            <ul className={styles.nav_logo}>
-              <Link to="/">Logo</Link>
-            </ul>
-            <ul className={styles.nav_menus}>
-              <li>
-                <Link to="/" className={styles.nav_menu}>
-                  상품추천
-                </Link>
-              </li>
-              <li>
-                <Link to="/product" className={styles.nav_menu}>
-                  제품보기
-                </Link>
-              </li>
-              <li>
-                <Link to="/notice" className={styles.nav_menu}>
-                  공지사항
-                </Link>
-              </li>
-              <li>
-                <Link to="/cart" className={styles.nav_menu}>
-                  장바구니
-                </Link>
-              </li>
-              <li>
-                <Link to="/mypage" className={styles.nav_menu}>
-                  마이페이지
-                </Link>
-              </li>
-            </ul>
-            <ul className={styles.nav_login}>
-              <Link to="/kakaologin" className={styles.nav_menu}>
-                로그인
-              </Link>
-            </ul>
-            <ul
-              className={styles.nav_toggle_btn}
-              onClick={() => {
-                setIsToggled(!isToggled);
-              }}
-            >
-              {" "}
-              <a href="#!" className={styles.nav_toggle_btn_text}>
-                Menu
-              </a>
-            </ul>
-          </nav>
+        <div className={styles.navbar_main}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <Link to="/">
+              <img
+                src="/img/main_logo_2.png"
+                style={{ width: "140px", marginTop: "22px" }}
+              />
+            </Link>
+          </div>
+          <div>
+            <nav className={styles.navbar}>
+              <div>
+                <ul className={styles.nav_menus}>
+                  <li>
+                    <Link to="/recommend" className={styles.logout_nav_menu}>
+                      상품추천
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/product" className={styles.logout_nav_menu}>
+                      제품보기
+                    </Link>
+                  </li>
 
+                  <li>
+                    <Link to="/notice" className={styles.logout_nav_menu}>
+                      공지사항
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/cart" className={styles.logout_nav_menu}>
+                      장바구니
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/kakaologin" className={styles.nav_menu}>
+                      <span
+                        style={{ cursor: "pointer" }}
+                        onClick={clickLogout}
+                        className={styles.logout_nav_menu}
+                      >
+                        로그인
+                      </span>
+                    </Link>
+                  </li>
+                </ul>
+                <ul
+                  className={styles.nav_toggle_btn}
+                  onClick={() => {
+                    setIsToggled(!isToggled);
+                  }}
+                >
+                  {" "}
+                  <a href="#!" className={styles.nav_toggle_btn_text}>
+                    Menu
+                  </a>
+                </ul>
+              </div>
+            </nav>
+          </div>
           {isToggled ? (
             <ul className={styles.toggle_menus}>
               <li className={styles.toggle_menu}>

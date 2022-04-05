ec8d85b3838b7dc7cd700ead5db1465788d2f33c
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-04-05 22:30:39 +0900

    feat: "리뷰 상세 모달 구현 및 버그 수정"

--- repill-frontend/src/components/main/mypage/myorder/ReviewDetailModal.js
+++ repill-frontend/src/components/main/mypage/myorder/ReviewDetailModal.js
@@ -5,7 +5,7 @@ import Rating from '@mui/material/Rating';
 import { useMediaQuery } from 'react-responsive';
 import axios from "axios"
 
-function ReviewDetailModal({ open, setOpen, id }) {
+function ReviewDetailModal({ open, setOpen, reviewId, productId, title, imgUrl }) {
   let token = sessionStorage.getItem('token')
   const headers = {
     Authorization: `Bearer ${token}`
@@ -13,30 +13,26 @@ function ReviewDetailModal({ open, setOpen, id }) {
   const [activate, setActivate] = useState(false)
   const [form, setForm] = useState("")
   const [review, setReview] = useState([])
-  const [value, setValue] = useState(2);
+  const [value, setValue] = useState(3);
   const isMobile = useMediaQuery({
     query: "(max-width : 768px)"
   });
-  const cancleReview = () => {
-    setOpen((prev) => !prev)
-  }
 
+  // 수정 버튼누르면 수정 가능하게 상태를 변경하는 함수
   const handleActivate = () => {
     setActivate((prev) => !prev)
-    if (activate === false) {
-      axios.put(`http://127.0.0.1:8000/api/v1/products/reviews/${id}/`, {
-
-      },
-      {
-        headers: headers
-      })
-    }
+    setValue("")
   }
 
   const deleteReview = () => {
-    axios.delete(`http://127.0.0.1:8000/api/v1/products/reviews/${id}/`, {
+    axios.delete(`http://127.0.0.1:8000/api/v1/products/reviews/${reviewId}/`, {
+      product: productId
+    },
+    {
       headers: headers
     })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
     window.location.reload(true)
   }
 
@@ -47,15 +43,25 @@ function ReviewDetailModal({ open, setOpen, id }) {
 
   // 수정된 리뷰 put 요청
   const editReview = () => {
-    console.log("수정완료")
+    if (form != review.content || value != review.rating)
+    axios.put(`http://127.0.0.1:8000/api/v1/products/reviews/${reviewId}/`, {
+      content: form,
+      rating: value,
+      product: productId
+    },{
+      headers: headers
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+    setTimeout(window.location.reload(true), 1000)
   }
 
   const getReview = async () => {
     try {
-      const response = await axios.get(`http://127.0.0.1:8000/api/v1/products/reviews/${id}/`, {
+      const response = await axios.get(`http://127.0.0.1:8000/api/v1/products/reviews/${reviewId}`, {
         headers: headers
       })
-      console.log(response.data)
+      setValue(response.data.rating)
       setReview(response.data)
     } catch(err) {
       console.log(err)
@@ -78,30 +84,50 @@ function ReviewDetailModal({ open, setOpen, id }) {
           >
             <Modal.Content>
               <div style={{ border: "1px solid black", height: "40%", marginTop: "5%", display: "flex", alignItems: "center", borderRadius: "10px" }}>
-                <div style={{ width: "20%" }}>
-                  <img src={"/assets/logo512.png"} alt=""  style={{ width: "70%", height: "100%" }} />
-                </div>
-                <div style={{ width: "60%" }}>
+                <div style={{ width: "70%", marginLeft: "2%" }}>
                   <div style={{ fontSize: "1rem" }}>
-                    
+                    {title}
                   </div>
                 </div>
               </div>
             </Modal.Content>
-            <Rating
-              name="simple-controlled"
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-              }} style={{ marginLeft: "5%"}} />
+            {activate ? (
+              <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }} style={{ marginLeft: "5%"}} />
+              ) : (
+                <Rating
+                name="simple-controlled"
+                value={value}
+                onChange={(event, newValue) => {
+                  setValue(newValue);
+                }} style={{ marginLeft: "5%"}} readOnly />
+              )}
             <Modal.Content>
               <Form>
-                <TextArea rows={10} style={{ fontSize: "1rem" }}></TextArea>
+                {activate ? (
+                    <TextArea rows={10} style={{ fontSize: "1rem" }} onChange={editContent} value={form}></TextArea>
+                  ) : (
+                    <TextArea rows={10} style={{ fontSize: "1rem" }} value={review.content} readOnly></TextArea>
+                  )}
               </Form>
             </Modal.Content>
             <Modal.Content style={{ display: "flex", justifyContent: "center" }}>
-              <button className={styles.order_add_reviewModal_button_mob}>수정</button>
-              <button onClick={cancleReview} className={styles.order_cancleModal_review_button_mob}>삭제</button>
+              <button onClick={handleActivate} className={styles.order_add_reviewModal_button_mob}>
+                {activate ? (
+                  <div onClick={editReview}>
+                    완료
+                  </div>
+                ) : (
+                  <div>
+                    수정
+                  </div>
+                )}
+              </button>
+              <button onClick={deleteReview} className={styles.order_cancleModal_review_button_mob}>삭제</button>
             </Modal.Content>
           </Modal>
         </>) : (
@@ -114,23 +140,23 @@ function ReviewDetailModal({ open, setOpen, id }) {
           >
             <Modal.Content>
               <div style={{ border: "1px solid black", height: "40%", display: "flex", alignItems: "center", borderRadius: "10px" }}>
-                <div style={{ width: "20%" }}>
-                  <img src={"/assets/logo512.png"} alt=""  style={{ width: "90%", height: "100%" }} />
+                <div style={{ width: "20%", marginLeft: "2%" }}>
+                  <img src={imgUrl} alt=""  style={{ width: "90%", height: "100%" }} />
                 </div>
-                <div style={{ width: "70%" }}>
-                  <div style={{ fontSize: "2rem" }}>
-                    제목
+                <div style={{ width: "70%", marginLeft: "2%" }}>
+                  <div style={{ fontSize: "2rem"  }}>
+                    {title}
                   </div>
                   {activate ? (
                     <Rating
                       name="simple-controlled"
-                      value={review.rating}
+                      value={value}
                       onChange={(event, newValue) => {
                         setValue(newValue);
                     }} size="large" style={{ marginTop: "3%" }} />) : (
                     <Rating
                       name="simple-controlled"
-                      value={review.rating}
+                      value={value}
                       onChange={(event, newValue) => {
                         setValue(newValue);
                     }} size="large" style={{ marginTop: "3%" }} readOnly />
@@ -141,7 +167,7 @@ function ReviewDetailModal({ open, setOpen, id }) {
             <Modal.Content>
               <Form>
                 {activate ? (
-                  <TextArea rows={10} style={{ fontSize: "1.5rem" }} onChange={editContent} value={review.content}></TextArea>
+                  <TextArea rows={10} style={{ fontSize: "1.5rem" }} onChange={editContent} value={form}></TextArea>
                 ) : (
                   <TextArea rows={10} style={{ fontSize: "1.5rem" }} value={review.content} readOnly></TextArea>
                 )}

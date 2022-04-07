09619da03e3dc51cc85325933ebec3fc0555d586
1gj2wo3tjr <1gj2wo3tjr@gmail.com>
2022-04-07 10:12:15 +0900

    test: "SurveyJs 라이브러리 테스트 코드"

--- repill-frontend/src/components/main/survey/SurveyPage.js
+++ repill-frontend/src/components/main/survey/SurveyPage.js
@@ -6,37 +6,68 @@ import { Survey, StylesManager, Model } from "survey-react";
 StylesManager.applyTheme("modern");
 
 function SurveyPage() {
+  const [sb, setSb] = useState([])  // sb에 주관식 2개 있음
+  const [ob, setOb] = useState([])
+  const element = []
+  const choices = [[],[],[],[],[],[],[],[],[]]
+
+  // 주관식 filter된 배열을 하나씩 꺼내서 저장
+  for(let i=0; i<sb.length; i++) {
+    element.push({
+      name: `${sb[i].id}`,
+      title: `${sb[i].content}`,
+      type: "text"
+    })
+  }
+
+
+  // 객관식 filter된 배열을 하나씩 꺼내서 저장
+  for(let i=0; i<ob.length; i++) {
+    for(let j=0; j<ob[i].choices.length; j++) {
+      choices[i].push(ob[i].choices[j].content)
+    }
+    element.push({
+      "type": "checkbox",
+      "name": "opSystem",
+      "title": `${ob[i].content}`,
+      "hasOther": true,
+      "isRequired": true,
+      "choices": choices[i]
+    })
+  }
+
   const surveyJson = {
-    title: "Software developer survey.",
-      pages: [
-          {
-              "title": "What operating system do you use?",
-              "elements": [
-                  {
-                      "type": "checkbox",
-                      "name": "opSystem",
-                      "title": "OS",
-                      "hasOther": true,
-                      "isRequired": true,
-                      "choices": ["Windows", "Linux", "Macintosh OSX"]
-                  }
-              ]
-          }]
+    elements: element,
+    // showProgressBar: "bottom",
+    // goNextPageAutomatic: false,
+    // showNavigationButtons: true,
+    // showPreviewBeforeComplete: true,
+    // pageNextText: "next"
   };
+
+
   const survey = new Model(surveyJson);
-  survey.focusFirstQuestionAutomatic = false;
-  const alertResults = useCallback((sender) => {
-    const results = JSON.stringify(sender.data);
-    alert(results);
-  }, []);
-  survey.onComplete.add(alertResults);
+
+  // const survey = new Model(surveyJson);
+
+  // survey.focusFirstQuestionAutomatic = false;
+  // const alertResults = useCallback((sender) => {
+  //   const results = JSON.stringify(sender.data);
+  //   alert(results);
+  // }, []);
+  // survey.onComplete.add(alertResults);
 
   const [questions, setQuestions] = useState([])
 
   const getQuestions = async() => {
     try{
       const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/api/v1/survey/question/`)
-      console.log(response.data)
+      // 주관식
+      const sb = response.data.filter(item => item.is_descriptive===true)
+      setSb(sb)
+      // 객관식
+      const ob = response.data.filter(item => item.is_descriptive===false)
+      setOb(ob)
       setQuestions(response.data)
     } catch(err) {
       console.log(err)
@@ -50,7 +81,9 @@ function SurveyPage() {
   return (
     <>
       <div>
+
         <Survey model={survey} />
+ 
       </div>
     </>
   );
